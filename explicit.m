01ad3f313b5faa988eb99e79585b35c2893b75d5
Alison Peard <alison.peard@gmail.com>
2021-01-31 13:52:57 +0000

    explicit code

--- explicit.m
+++ explicit.m
@@ -1,6 +1,28 @@
 % Explicit method
-f = @(x) x^2;
-f(2)
-f(4)
-g = @(x,y) x+3*x^2;
-g(2,3)
+Tfinal = 10;
+N = 100; % number of timesteps to take
+h = Tfinal/N; % timestep
+k0 = 0;kstep=1;kend=5;
+
+for k = k0:kstep:kend
+
+    Tn = 1; U0 =1;
+    U = zeros(1,N);
+    steps = (0:N);
+    U(1) = U0;
+
+    for n=1:N
+        Un = U(n);
+        U(n+1) = Un + h * Un*(1-Un-Tn)/k;
+        Tn = Tn + h * (Un + U(n+1))/2;
+    end
+
+    plot(steps,U);
+    hold on;
+end
+
+title('Explicit solution with composite trapezoidal')
+K = k0:kstep:kend;
+legendStrings = "k = " + string(K);
+legend(legendStrings)
+
