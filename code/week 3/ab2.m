870a62ecd704f65ab164894e2b2884a5a84b67d2
alecsargood <58740397+alecsargood@users.noreply.github.com>
2021-02-13 17:43:34 +0000

    Update ab2.m

--- code/week 3/ab2.m
+++ code/week 3/ab2.m
@@ -7,9 +7,9 @@ function x1 = ab2(x1,x0, h, k)
 % fix order of y,u to match
     
     
-    func = @(v)[v(1);(v(2)/k)*(1-v(1)-v(2))];
+    func = @(v)[v(2);(v(2)/k)*(1-v(1)-v(2))];
         
-    x1 = x0 + (h/2)*((3*func(x1))-(func(x0)));
+    x1 = x1 + (h/2)*((3*func(x1))-(func(x0)));
     
 
     
